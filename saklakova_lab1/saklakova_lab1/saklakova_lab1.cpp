c136d4ed2cf581c5cdb26078f7aee7e9fde3a877
valerusyaaa <140447211+valerusyaaa@users.noreply.github.com>
2023-09-29 21:15:42 +0300

    labal1
    labal1 Saklakova Valeria

---
+++ saklakova_lab1/saklakova_lab1/saklakova_lab1.cpp
@@ -0,0 +1,41 @@
+﻿// saklakova_lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+//При запуске программы выводится меню, запрашивающее в бесконечном цикле действие от пользователя.Пример меню : 
+1. Добавить трубу 
+2. Добавить КС 
+3. Просмотр всех объектов 
+4. Редактировать трубу 
+5. Редактировать КС 
+6. Сохранить 
+7. Загрузить 
+//0. Выход
+void printed() 
+{
+cout << "1.Add pipe
+	2. Add compressor station"
+}
+	
+
+
+
+
+
+void main()
+{
+	struct truba {
+		string truba_name;
+		double len;
+		double diam;
+		bool rem;
+	} truba;
+
+	struct ks {
+		string ks_name;
+		double efc;
+		int ws;
+		int wws;
+	} ks;
+}
